48c33cf76ab744f847968e9b0d2f06800996af56
prapeller <prapeller@mail.ru>
2022-04-10 16:22:32 +0300

    add profile

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -110,98 +110,98 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST_FRAMEWORK = {
-#     "DEFAULT_RENDERER_CLASSES": [
-#         "rest_framework.renderers.JSONRenderer",
-#         # 'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
-#         # 'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-#     ],
-#     # 'DEFAULT_PARSER_CLASSES': (
-#     # If you use MultiPartFormParser or FormParser, we also have a camel case version
-#     # 'djangorestframework_camel_case.parser.CamelCaseFormParser',
-#     # 'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
-#     # 'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-#     # ),
-#     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-#     "PAGE_SIZE": 100,
-#
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         # permissions to makeOperations/toViewOnly for authenticated users in current session
-#         # "rest_framework.permissions.IsAuthenticated",
-#         # "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-#
-#         # permissions according to auth_permission
-#         "rest_framework.permissions.DjangoModelPermissions",
-#         # "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
-#
-#         # for making own permissions
-#         # "rest_framework.permissions.BasePermission",
-#     ],
-#
-#     "DEFAULT_VERSIONING_CLASS":
-#     # http://127.0.0.1:8000/api/v2/users/
-#         "rest_framework.versioning.URLPathVersioning",
-#
-#     # path('api/v1/users/', include('userapp.urls', namespace='v1')),
-#     # 'rest_framework.versioning.NamespaceVersioning',
-#
-#     # http://v1.example.com/bookings/
-#     # 'rest_framework.versioning.HostNameVersioning',
-#
-#     # http://127.0.0.1:8000/api/users/?version=v1/
-#     # 'rest_framework.versioning.QueryParameterVersioning',
-#
-#     # requests.get('http://127.0.0.1:8000/api/users/', headers={'Accept': 'application/json; version=v2'})
-#     # 'rest_framework.versioning.AcceptHeaderVersioning',
-#
 #     "EXCEPTION_HANDLER": "core_apps.common.exceptions.common_exception_handler",
 #     "NON_FIELD_ERRORS_KEY": "error",
-#
-#     "DEFAULT_AUTHENTICATION_CLASSES": [
-#         # passing session id (for debugging in browser)
-#         # 'rest_framework.authentication.SessionAuthentication',
-#
-#         # passing login/password in request.args (http authentification)
-#         # 'rest_framework.authentication.BasicAuthentication',
-#
-#         # passing token in headers
-#         # 'rest_framework.authentication.TokenAuthentication',
-#
-#         # jwt-based authentication
-#         "rest_framework_simplejwt.authentication.JWTAuthentication",
-#     ],
+    # "DEFAULT_RENDERER_CLASSES": [
+    #     "rest_framework.renderers.JSONRenderer",
+        # 'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        # 'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    # ],
+    # 'DEFAULT_PARSER_CLASSES': (
+    # If you use MultiPartFormParser or FormParser, we also have a camel case version
+    # 'djangorestframework_camel_case.parser.CamelCaseFormParser',
+    # 'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+    # 'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    # ),
+    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    # "PAGE_SIZE": 100,
+
+    # "DEFAULT_PERMISSION_CLASSES": [
+        # permissions to makeOperations/toViewOnly for authenticated users in current session
+        # "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+
+        # permissions according to auth_permission
+        # "rest_framework.permissions.DjangoModelPermissions",
+        # "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+
+        # for making own permissions
+        # "rest_framework.permissions.BasePermission",
+    # ],
+
+    # "DEFAULT_VERSIONING_CLASS":
+    # http://127.0.0.1:8000/api/v2/users/
+    #     "rest_framework.versioning.URLPathVersioning",
+
+    # path('api/v1/users/', include('userapp.urls', namespace='v1')),
+    # 'rest_framework.versioning.NamespaceVersioning',
+
+    # http://v1.example.com/bookings/
+    # 'rest_framework.versioning.HostNameVersioning',
+
+    # http://127.0.0.1:8000/api/users/?version=v1/
+    # 'rest_framework.versioning.QueryParameterVersioning',
+
+    # requests.get('http://127.0.0.1:8000/api/users/', headers={'Accept': 'application/json; version=v2'})
+    # 'rest_framework.versioning.AcceptHeaderVersioning',
+
+
+    # "DEFAULT_AUTHENTICATION_CLASSES": [
+        # passing session id (for debugging in browser)
+        # 'rest_framework.authentication.SessionAuthentication',
+
+        # passing login/password in request.args (http authentification)
+        # 'rest_framework.authentication.BasicAuthentication',
+
+        # passing token in headers
+        # 'rest_framework.authentication.TokenAuthentication',
+
+        # jwt-based authentication
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+    # ],
 # }
 
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": (
-        "Bearer",
-        "JWT",
-    ),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    # "SIGNING_KEY": env("SIGNING_KEY"),
-    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-}
+# SIMPLE_JWT = {
+#     "AUTH_HEADER_TYPES": (
+#         "Bearer",
+#         "JWT",
+#     ),
+#     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+#     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+#     # "SIGNING_KEY": env("SIGNING_KEY"),
+#     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+#     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+# }
 
-DJOSER = {
-    "LOGIN_FIELD": "email",
-    "USER_CREATE_PASSWORD_RETYPE": True,
-    "USERNAME_CHANGED_EMAIL_CONFIMATION": True,
-    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
-    "SEND_CONFIRMATION_EMAIL": True,
-    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
-    "SET_PASSWORD_RETYPE": True,
-    "PASSWORD_RESET_CONFIRM_RETYPE": True,
-    "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
-    "ACTIVATION_URL": "activate/{uid}/{token}",
-    "SEND_ACTIVATION_EMAIL": True,
-    "SERIALIZERS": {
-        "user_create": "core_apps.users.serializers.CreateUserSerializer",
-        "user": "core_apps.users.serializers.UserSerializer",
-        "current_user": "core_apps.users.serializers.UserSerializer",
-        "user_delete": "djoser.serializers.UserDeleteSerializer",
-    },
-}
+# DJOSER = {
+#     "LOGIN_FIELD": "email",
+#     "USER_CREATE_PASSWORD_RETYPE": True,
+#     "USERNAME_CHANGED_EMAIL_CONFIMATION": True,
+#     "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+#     "SEND_CONFIRMATION_EMAIL": True,
+#     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+#     "SET_PASSWORD_RETYPE": True,
+#     "PASSWORD_RESET_CONFIRM_RETYPE": True,
+#     "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
+#     "ACTIVATION_URL": "activate/{uid}/{token}",
+#     "SEND_ACTIVATION_EMAIL": True,
+#     "SERIALIZERS": {
+#         "user_create": "core_apps.users.serializers.CreateUserSerializer",
+#         "user": "core_apps.users.serializers.UserSerializer",
+#         "current_user": "core_apps.users.serializers.UserSerializer",
+#         "user_delete": "djoser.serializers.UserDeleteSerializer",
+#     },
+# }
 
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
---
+++ core_apps/common/exceptions.py
@@ -0,0 +1,27 @@
+from rest_framework.views import exception_handler
+
+
+def common_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    handlers = {
+        "ValidationError": _handle_generic_error,
+        "NotFound": _handle_not_found_error,
+    }
+
+    exception_class = exc.__class__.__name__
+    if exception_class in handlers:
+        return handlers[exception_class](exc, context, response)
+    return response
+
+
+def _handle_generic_error(exc, context, response):
+    response.data = {"status_code": response.status_code, "errors": response.data}
+    return response
+
+
+def _handle_not_found_error(exc, context, response):
+    view = context.get("view")
+    if view and hasattr(view, "queryset") and view.queryset is not None:
+        status_code = response.status_code
+        error_key = view.queryset.model._meta.verbose_name
+        response.data = {"status_code": status_code, "errors": {error_key: response.data.get("detail")}}
