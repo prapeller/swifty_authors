76b6beb37c709ddd7c2675695225f27402f03845
prapeller <prapeller@mail.ru>
2022-04-15 16:54:33 +0300

    add trailing slashes

--- core_apps/profiles/urls.py
+++ core_apps/profiles/urls.py
@@ -7,8 +7,8 @@ from .views import (
 
 urlpatterns = [
     path("list/", ProfileListAPIView.as_view(), name="profile-list"),
-    path("detail/<str:username>", ProfileDetailAPIView.as_view(), name="profile-detail"),
-    path("update/<str:username>", UpdateProfileAPIView.as_view(), name="profile-update"),
-    path("followers/<str:username>", get_followers, name="profile-followers"),
-    path("follow-unfollow/<str:username>", FollowUnfollowAPIView.as_view(), name="profile-follow-unfollow"),
+    path("detail/<str:username>/", ProfileDetailAPIView.as_view(), name="profile-detail"),
+    path("update/<str:username>/", UpdateProfileAPIView.as_view(), name="profile-update"),
+    path("followers/<str:username>/", get_followers, name="profile-followers"),
+    path("follow-unfollow/<str:username>/", FollowUnfollowAPIView.as_view(), name="profile-follow-unfollow"),
 ]
